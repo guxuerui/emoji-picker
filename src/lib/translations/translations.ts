9430203fde7d47655cfdaf1bb3df68ec5ad6647c
guxuerui <im.guxuerui@gmail.com>
2023-04-13 23:22:54 +0800

    🌟 feat(translations.ts): add copyToast translation for homepage emoji copy confirmation message

--- src/lib/translations/translations.ts
+++ src/lib/translations/translations.ts
@@ -4,11 +4,13 @@ export default {
     "homepage.nodata": "No results...",
     "homepage.search": "Search emoji...",
     "homepage.hint": "Click to pick an emoji...",
+    "homepage.copyToast": "Emoji shortcode copied to clipboard!",
   },
   cn: {
     "homepage.title": "搜索Github表情",
     "homepage.nodata": "没有结果...",
     "homepage.search": "搜索表情...",
     "homepage.hint": "点击选择表情...",
+    "homepage.copyToast": "表情符号已复制到剪贴板!",
   },
 };
