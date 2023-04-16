ff28b7f952a4c18789bce262c539658905af6a75
guxuerui <im.guxuerui@gmail.com>
2023-04-16 14:48:41 +0800

    🎉 feat(Footer.svelte): add footer component with link to author's blog

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,15 @@
+<div mt-4 text-gray-600 dark:text-gray-300>
+  Made by
+  <a
+    border="0 b-1 dashed #aaa"
+    hover:border="solid #000"
+    dark:hover:border="solid #fff"
+    class="page-title transition-colors"
+    rel="noreferrer"
+    href="https://www.guxuerui.cn"
+    target="_blank"
+    title="MyBlog"
+  >
+    guxuerui
+  </a>
+</div>
